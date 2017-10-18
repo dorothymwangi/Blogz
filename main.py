d85cc3440a217a5ae187409d05307c73d1cd28ad
Dorothy Mwangi <dmwangiw@gmail.com>
2017-10-17 21:31:44 -0500

    Final working code

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Blog(db.Model):
         self.body = body
 
 @app.route('/blog')
-def blog():
+def index():
 
     if request.args:
         blog_id = request.args.get('id')
@@ -28,43 +28,39 @@ def blog():
         blogs = Blog.query.all()
         return render_template('blog.html', title='My blogs', blogs=blogs)    
 
-@app.route('/newpost')
-def index():
-    return render_template('newpost.html', title='Build a Blog') 
+#@app.route('/newpost')
+#def index():
+  #  return render_template('newpost.html', title='Build a Blog') 
 
-@app.route('/newpost', methods = ['POST'])
+@app.route('/newpost', methods = ['POST','GET'])
 def newpost():
-    #if request.method == 'POST':
-    blog_name = request.form['blog']
-    blog_body = request.form['body']
-    blog_name_error = ""
-    blog_body_error = ""
-
-    if blog_name == "":
-        blog_name_error="Please enter a blog name"
-    if blog_body == "":
-        blog_body_error="Please enter blog content"
-
-    if not blog_name_error and not blog_body_error:
-        new_blog_id = None
-        new_blog = Blog(blog_name, blog_body)
-        db.session.add(new_blog)
-        db.session.commit() 
-
-        new_blog_id = request.args.get('id')
-        blog=Blog.query.get(new_blog_id)
-
-        return render_template('individualblog.html', blog=blog)
-        #return redirect('/blog')
-    else:
-        return render_template('newpost.html',blog=blog_name, body=blog_body, 
-           blog_name_error=blog_name_error, blog_body_error=blog_body_error)
     
-@app.route('/individualblog')
-def single():
-    blog = request.args.get('blog')
-    body = request.args.get('body')
-    return render_template ('individualblog.html', blog=blog, body=body)
+    if request.method == 'GET':
+        return render_template('newpost.html', title='Build a Blog')
 
+    if request.method == 'POST':
+        blog_name = request.form['blog']
+        blog_body = request.form['body']
+        blog_name_error = ""
+        blog_body_error = ""
+
+        if blog_name == "":
+            blog_name_error="Please enter a blog name"
+        if blog_body == "":
+            blog_body_error="Please enter blog content"
+
+        if not blog_name_error and not blog_body_error:
+            new_blog = Blog(blog_name, blog_body)
+            db.session.add(new_blog)
+            db.session.commit() 
+
+            blog=Blog.query.get('/blog?id='+ str(new_blog.id))
+
+            return render_template('individualblog.html', blog=new_blog)
+            #return redirect('/blog')
+        else:
+            return render_template('newpost.html',blog=blog_name, body=blog_body, 
+            blog_name_error=blog_name_error, blog_body_error=blog_body_error)       
+    
 if __name__ =='__main__':
     app.run()
